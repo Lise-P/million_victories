68cec53e8990100741a6b865c168416fe0cceae8
Lise PERARD <liseprd@hotmail.fr>
2025-02-06 16:56:11 +0100

    fonction recherche

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import router from "./routes/router";
-import showRepository from "./showRepository"; 
+import showRepository from "./showRepository";
 
 dotenv.config();
 const app = express();
